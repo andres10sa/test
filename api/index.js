e1e232749f0ba6f3542d2f9d39d39bb8e43f917f
andres10sa <camand10sa@gmail.com>
2021-01-24 21:06:19 -0500

    readme

--- api/index.js
+++ api/index.js
@@ -3,15 +3,6 @@ const fs = require("fs");
 
 let data_test={results:[]};
 
-// const getUsers = async () => {
-//     try {
-//         const { data: { results } } = await axios.get("https://randomuser.me/api/?results=10");
-//         data.results = results;
-//     } catch (error) {
-//         console.log('GET USERS ERROR:', error)
-//     }
-// }
-
 const getData = async (url) => {
     try {
         const { data: { results } } = await axios.get(url);
